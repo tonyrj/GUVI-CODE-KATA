f70dfdeeab2bd4a9109a4c0f5942ec82b688d2fd
Ramkumar <ram7953@gmail.com>
2019-02-18 23:34:27 +0530

    Update sumofsq.c

--- sumofsq.c
+++ sumofsq.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
+
+
 int main()
 {
-int a,m,r,s;
-scanf("%d",&a);
-m=a%10;
-r=a/10;
-s=(m*m)+(r*r);
-printf("%d",s);
-return 0;
+ int n,t=0,r,q;
+
+ scanf("%d",&n);
+ q=n;
+ do
+ {
+      r=q%10;
+	t += r*r;
+      q=q/10;
+ }
+ while(q%10!=0);
+ printf("%d",t);
+
 }
